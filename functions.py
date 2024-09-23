349202d5fddf029fe1eb81f7bcc58a7bafd083df
tommy <tommasominuto@gmail.com>
2024-09-23 22:02:05 +0200

    fix

--- functions.py
+++ functions.py
@@ -222,4 +222,4 @@ def perform_transcription(audio_source, transcription_option, api_keys, selected
     except Exception as e:
         st.error(f"Si è verificato un errore durante la trascrizione: {str(e)}")
     
-    return full_transcript, summary
+    return full_transcript
